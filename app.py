ae2e5a61fffe6c6d2827be6b122c1e621f395900
Salim Oyinlola <salimoyinlola@gmail.com>
2024-05-20 14:46:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def run_arctic_instruct(prompt, temperature, replicate_api_token):
     # Get API token from environment variable (replace with your actual variable name)
     replicate_api_token = replicate_api_token
     if not replicate_api_token:
-      st.error("REPLICATE_API_TOKEN environment variable not set!")
+      st.error("SNOWFLAKE ARTIC INSTRUCT API TOKEN NOT SET!")
       return
 
     input_data = {
@@ -58,27 +58,38 @@ def run_arctic_instruct(prompt, temperature, replicate_api_token):
 
 # Define the Streamlit app
 def main():
-    st.title("Plant Status Predictor")
-    
+    st.title("AgriTracka")
+    st.subheader("Your Farm's Digital Guardian")
+    st.markdown("""
+        Welcome to AgriTracka! Please input the light intensity, temperature, and humidity levels of your farm's environment. 
+       Provide your Snowflake Arctic Instruct API key to get personalized advice for maintaining your plant.
+        """)    
     # Input fields for user
-    light_intensity = st.number_input("Light Intensity", min_value=0, max_value=15000, step=1)
-    temperature = st.number_input("Temperature", min_value=0, max_value=50, step=1)
-    humidity = st.number_input("Humidity", min_value=0, max_value=100, step=1)
-    api_key = st.text_input("Enter API Key")
+    light_intensity = st.number_input("Light Intensity (in lux)", min_value=0, max_value=15000, step=1, help="Enter the current light intensity in lux.")
+    temperature = st.number_input("Temperature (in °C)", min_value=0, max_value=50, step=1, help="Enter the current temperature in degrees Celsius.")
+    humidity = st.number_input("Humidity (in %)", min_value=0, max_value=100, step=1, help="Enter the current humidity percentage.")
+    api_key = st.text_input("Enter API Key", type="password", help="Enter your API key to get advice.")
 
-    # Make prediction
+
+    # Ensure inputs are valid
     if st.button('Predict Plant Status and Advice the Farmer'):
-        prediction = model.predict([[light_intensity, temperature, humidity]])
-        # Decode the predicted label
-        predicted_status = label_encoder.inverse_transform(prediction)
-        # Display prediction
-        predicted_status = predicted_status[0]
-        st.write("Predicted Plant Status:", predicted_status)
-    
-        prompt_text = "As an agriculturist, give advice to an African farmer on how to deal with a " + predicted_status + " plant."
-        temperature = 0.8
-        response = run_arctic_instruct(prompt_text, temperature, api_key)
-        full_response = st.write_stream(response)
+        if not api_key:
+            st.error("Please enter a valid API key.")
+        else:
+          try:
+            prediction = model.predict([[light_intensity, temperature, humidity]])
+            # Decode the predicted label
+            predicted_status = label_encoder.inverse_transform(prediction)
+            # Display prediction
+            predicted_status = predicted_status[0]
+            st.write("Predicted Plant Status:", predicted_status)
+        
+            prompt_text = "As an agriculturist, give advice to an African farmer on how to deal with a " + predicted_status + " plant."
+            temperature = 0.8
+            response = run_arctic_instruct(prompt_text, temperature, api_key)
+            full_response = st.write_stream(response)
+          except Exception as e:
+                st.error(f"An error occurred: {e}")
 
 
 # Run the app
